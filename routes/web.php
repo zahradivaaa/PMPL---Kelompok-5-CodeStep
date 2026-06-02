8014cfeab256c87e2757f564eb76574db8e000b6
Nuraliza <nuralizashafira29@gmail.com>
2026-06-02 13:09:27 +0700

    membuat halaman profile guru

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/kategori/{slug}', [KategoriController::class, 'show'])
 // Route Guru
 Route::middleware(['auth', 'guru'])->prefix('guru')->name('guru.')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Guru\GuruDashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [App\Http\Controllers\Guru\GuruProfileController::class, 'index'])->name('profile');
 });
 
 require __DIR__.'/auth.php';
